1d829baa17668c9512685480f0576356608f038b
FWJ <fangwj@FWJdeMacBook-Air.local>
2026-07-05 13:27:47 +0700

    Connect site to Supabase backend

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.CALON_SUPABASE = {
-    url: "",
-    anonKey: "",
+    url: "https://elsprhwlfzmatlvzkuhq.supabase.co",
+    anonKey: "sb_publishable_ZXJrzCJhqGw0z3_jd90-YA_GdL5gvw7",
     requireFrontendLogin: false
 };
